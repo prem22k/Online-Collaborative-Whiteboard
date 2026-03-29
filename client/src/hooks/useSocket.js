457da732d88ef7d1243c65a984a84db2fb9a4a7d
prem22k <premsai224k@gmail.com>
2026-03-29 14:19:57 +0530

    feat(Client): extract useSocket hook for Socket.io connection and event handling

---
+++ client/src/hooks/useSocket.js
@@ -0,0 +1,27 @@
+import { useEffect, useRef } from 'react';
+import { io } from 'socket.io-client';
+import { SERVER_URL } from '../utils/constants';
+
+export default function useSocket(canvasRef) {
+  const socketRef = useRef(null);
+
+  useEffect(() => {
+    socketRef.current = io(SERVER_URL);
+
+    socketRef.current.on('draw', (data) => {
+      canvasRef.current?.drawRemoteStroke(data);
+    });
+
+    socketRef.current.on('undo', () => {
+      canvasRef.current?.triggerRemoteUndo();
+    });
+
+    return () => {
+      socketRef.current.off('draw');
+      socketRef.current.off('undo');
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  return socketRef;
+}
