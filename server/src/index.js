d3ccd9a028dc47284fb9a55712c47c7b4d90f68b
mythri105 <mythrikolipelli@gmail.com>
2026-03-29 13:22:18 +0530

    refactor(Server): move EventQueue and Express entry point into structured directories

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const EventQueue = require('./eventQueue.js');
+const EventQueue = require('./utils/EventQueue.js');
 
 const app = express();
 const server = http.createServer(app);
