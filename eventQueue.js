2a55ccf73677e09a37e99465a1079730463d3f8d
mythri105 <mythrikolipelli@gmail.com>
2026-03-29 13:22:15 +0530

    feat(Queue): add peek, isEmpty, size, and toArray auxiliary methods

--- eventQueue.js
+++ eventQueue.js
@@ -11,11 +11,33 @@ class EventQueue {
     }
 
     dequeue() {
-        if (this.head === this.tail) return undefined;
+        if (this.isEmpty()) return undefined;
+        
         const item = this.items[this.head];
         delete this.items[this.head];
         this.head++;
         return item;
+    }
+
+    peek() {
+        if (this.isEmpty()) return undefined;
+        return this.items[this.head];
+    }
+
+    isEmpty() {
+        return this.size() === 0;
+    }
+
+    size() {
+        return this.tail - this.head;
+    }
+
+    toArray() {
+        const arr = [];
+        for (let i = this.head; i < this.tail; i++) {
+            arr.push(this.items[i]);
+        }
+        return arr;
     }
 }
 module.exports = EventQueue;
